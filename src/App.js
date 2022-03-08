00fd6634564066e03b24b905a797ef0566823496
Andreyrvs <cortaesai@gmail.com>
2022-03-08 13:03:27 -0300

    Iniciando Receitas em progresso

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+// import rockGlass from './images/rockGlass.svg';
+import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import MyProvider from './context/provider';
 import Routes from './pages/Routes';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const newHistory = createBrowserHistory();
   return (
-    <BrowserRouter history={ newHistory }>
+    <Router history={ newHistory }>
       <MyProvider>
-        <div className="meals">
-          <Routes />
-          <span className="logo">TRYBE</span>
-          <object
-            className="rocksGlass"
-            type="image/svg+xml"
-            data={ rockGlass }
-          >
-            Glass
-          </object>
-        </div>
+        <Routes />
       </MyProvider>
-    </BrowserRouter>
+    </Router>
   );
 }
 
