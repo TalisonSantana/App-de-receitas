299e7f4b1c33b5f463c971d7600b19c4bca80e91
Andreyrvs <cortaesai@gmail.com>
2022-03-08 23:23:25 -0300

    Featre (DrinkProgress): Requisito 47 - Adiciona todos itens do requsito com respectivos data-testid

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -50,7 +50,7 @@ function Routes() {
       <Route exact path="/explore/drinks" component={ ExploreDrinks } />
       <Route exact path="/explore/foods" component={ ExploreFoods } />
       <Route exact path="/explore" component={ Explore } />
-      <Route exact path="/drinks/:idDaReceita/in-progress" component={ DrinkProgress } />
+      <Route exact path="/drinks/idDaReceita/in-progress" component={ DrinkProgress } />
       <Route exact path="/foods/idDaReceita/in-progress" component={ FoodProgress } />
       <Route exact path="/drinks/:idDaReceita" component={ DrinkRecipe } />
       <Route exact path="/foods/:idDaReceita" component={ FoodRecipe } />
--- src/services/index.js
+++ src/services/index.js
@@ -10,6 +10,18 @@ const FetchEats = async (url) => {
   }
 };
 
+export const FetchDrinks = async (url) => {
+  const API = url;
+  try {
+    const results = await (await fetch(API)).json();
+    const { drinks } = results;
+    console.log(drinks);
+    return drinks;
+  } catch (error) {
+    return error;
+  }
+};
+
 // FetchEats('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
 
 export default FetchEats;
