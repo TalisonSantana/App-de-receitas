3f6e259dfb82459ba28f8ef8fe6f50c4b22620ae
Vinicius Jacques <vinicius-jacques@hotmail.com>
2022-03-09 17:43:14 -0300

    Requito 17

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import CardsRecipes from '../components/CardsRecipes';
 import Footer from '../components/Footer';
 import Header from '../components/header/Header';
 
 function Drinks() {
   return (
-    <>
+    <main>
       <Header
         title="Drinks"
         searchIcon
       />
+      <CardsRecipes />
       <Footer />
-    </>
+    </main>
   );
 }
 
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CardsRecipes from '../components/CardsRecipes';
 import Footer from '../components/Footer';
 import Header from '../components/header/Header';
 
@@ -9,6 +10,7 @@ function Foods() {
         title="Foods"
         searchIcon
       />
+      <CardsRecipes />
       <Footer />
     </>
   );
