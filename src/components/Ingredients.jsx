35921caa03c5fca39d946dcbf87b66dab845936e
Andreyrvs <cortaesai@gmail.com>
2022-03-09 20:36:38 -0300

    Feat (Food Progress): Faz um componente com as  informaçoes da comida

--- src/components/Ingredients.jsx
+++ src/components/Ingredients.jsx
@@ -1,34 +1,81 @@
 import React, { } from 'react';
 import PropTypes from 'prop-types';
 // import MyContext from '../context';
+import shareIcon from '../images/shareIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 
 function Ingredients({ item }) {
-  const keys = Object.keys(item);
-  const myRegex = /strIngredient/gi;
-  const filterWithRegex = keys.filter((el) => el.match(myRegex));
+  const recipe = () => {
+    const keys = Object.keys(item);
+    const myRegex = /strIngredient/gi;
+    const filterWithRegex = keys.filter((el) => el.match(myRegex));
+    const valores = filterWithRegex.map((el) => item[el]);
 
-  // console.log('keys', item);
-  // console.log('filterWithRegex', filterWithRegex);
-  // console.log(filterWithRegex.map((el) => item[el]));
-
-  const valores = filterWithRegex.map((el) => item[el]);
-
-  return (
-    <section className="d-flex flex-column">
-      {valores.map((elemento, index) => (
-        elemento
+    return (
+      <section className="d-flex flex-column">
+        {valores.map((elemento, index) => (
+          elemento
         && (
-          <section>
+          <section key={ index }>
             <input type="checkbox" />
             <span
-              key={ index }
               data-testid={ `${index}-ingredient-step` }
             >
               {elemento}
             </span>
           </section>
         )
-      ))}
+        ))}
+      </section>
+    );
+  };
+
+  return (
+    <section className="receita">
+      <section className="container__image">
+        <img
+          className="image"
+          data-testid="recipe-photo"
+          src={ item.strMealThumb }
+          alt="strMealThumb"
+        />
+      </section>
+      <section className="container__title-buttons">
+        <section className="container__title">
+          <span data-testid="recipe-title">{item.strMeal}</span>
+          <span data-testid="recipe-category">{item.strCategory}</span>
+        </section>
+        <section className="container__buttons">
+          <img
+            height="26px"
+            data-testid="share-btn"
+            src={ shareIcon }
+            alt="shareIcon"
+          />
+          <img
+            height="26px"
+            data-testid="favorite-btn"
+            src={ whiteHeartIcon }
+            alt="shareIcon"
+          />
+        </section>
+      </section>
+      <section className="container__ingredientes">
+        <p>Ingredientes</p>
+        {recipe()}
+      </section>
+      <section className="container__instructions">
+        <span>Instructions</span>
+        <p
+          data-testid="instructions"
+          className="instructions"
+        >
+          {item.strInstructions}
+        </p>
+      </section>
+      <section className="container__button">
+        <button data-testid="finish-recipe-btn" type="button">Finish Recipe</button>
+      </section>
     </section>
   );
 }
--- src/pages/FoodProgress.jsx
+++ src/pages/FoodProgress.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import FetchEats from '../services';
-import shareIcon from '../images/shareIcon.svg';
-import whiteHeartIcon from '../images/whiteHeartIcon.svg';
-import Ingredients from '../components/Ingredients';
+// import shareIcon from '../images/shareIcon.svg';
+// import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import './css/FoodProgress.css';
+import Ingredients from '../components/Ingredients';
 
 function FoodProgress() {
   const [ingredients, setIngredients] = useState([]);
@@ -21,53 +21,9 @@ function FoodProgress() {
   return (
     <div>
       {ingredients.map((item, index) => (
-        <section key={ item.idMeal } className="receita">
-          <section className="container__image">
-            <img
-              className="image"
-              data-testid="recipe-photo"
-              src={ item.strMealThumb }
-              alt="strMealThumb"
-            />
-          </section>
-          <section className="container__title-buttons">
-            <section className="container__title">
-              <span data-testid="recipe-title">{item.strMeal}</span>
-              <span data-testid="recipe-category">{item.strCategory}</span>
-            </section>
-            <section className="container__buttons">
-              <img
-                height="26px"
-                data-testid="share-btn"
-                src={ shareIcon }
-                alt="shareIcon"
-              />
-              <img
-                height="26px"
-                data-testid="favorite-btn"
-                src={ whiteHeartIcon }
-                alt="shareIcon"
-              />
-            </section>
-          </section>
-          <section className="container__ingredientes">
-            <p>Ingredientes</p>
-            <Ingredients key={ index } item={ item } />
-          </section>
-          <section className="container__instructions">
-            <span>Instructions</span>
-            <p
-              data-testid="instructions"
-              className="instructions"
-            >
-              {item.strInstructions}
-            </p>
-          </section>
-          <section className="container__button">
-            <button data-testid="finish-recipe-btn" type="button">Finish Recipe</button>
-          </section>
-        </section>
+        <Ingredients key={ index } item={ item } />
       ))}
+
     </div>
 
   );
