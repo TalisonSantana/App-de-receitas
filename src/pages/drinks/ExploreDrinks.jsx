a19897de916459aa9463f5c1bbf0c0424cd80548
Vinicius Jacques <vinicius-jacques@hotmail.com>
2022-03-10 16:46:57 -0300

    Requisito 70 ao 73

--- src/pages/drinks/ExploreDrinks.jsx
+++ src/pages/drinks/ExploreDrinks.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
 
 function ExploreDrinks() {
+  const history = useHistory();
   return (
     <>
       <Header title="Explore Drinks" />
+      <Header title="Explore Foods" />
+      <button
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/drinks/ingredients') }
+        type="button"
+      >
+        By Ingredient
+
+      </button>
+      <button
+        data-testid="explore-surprise"
+        onClick={ () => history.push('/explore/drinks') }
+        type="button"
+      >
+        Surprise me!
+
+      </button>
       <Footer />
     </>
   );
--- src/pages/foods/ExploreFoods.jsx
+++ src/pages/foods/ExploreFoods.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
 
 function ExploreFoods() {
+  const history = useHistory();
   return (
     <>
       <Header title="Explore Foods" />
+      <button
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explore/foods/ingredients') }
+        type="button"
+      >
+        By Ingredient
+
+      </button>
+      <button
+        data-testid="explore-by-nationality"
+        onClick={ () => history.push('/explore/foods/nationalities') }
+        type="button"
+      >
+        By Nationality
+
+      </button>
+      <button
+        data-testid="explore-surprise"
+        onClick={ () => history.push('/explore/drinks') }
+        type="button"
+      >
+        Surprise me!
+
+      </button>
       <Footer />
     </>
   );
