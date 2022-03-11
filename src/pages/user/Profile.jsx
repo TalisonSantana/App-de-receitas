68e6bcdf4fe846e2fb68310d4e83d4894ed67b1c
Talison <talisonsantana76@gmail.com>
2022-03-11 18:45:13 -0300

    ajuste storage

--- src/pages/user/Profile.jsx
+++ src/pages/user/Profile.jsx
@@ -4,10 +4,10 @@ import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
 
 function Profile({ history }) {
-  const [email, setEmail] = useState('');
+  const [user, setUser] = useState('');
   useEffect(() => {
-    const data = localStorage.getItem('user');
-    setEmail(JSON.parse(data).email);
+    setUser(JSON.parse(localStorage.getItem('user'))
+    || { email: 'nao encontrado' });
   }, []);
 
   const handleClick = (path) => {
@@ -20,7 +20,7 @@ function Profile({ history }) {
     <>
       <Header title="Profile" />
       <section>
-        <h3 data-testid="profile-email">{email}</h3>
+        <h3 data-testid="profile-email">{user.email}</h3>
         <button
           type="button"
           data-testid="profile-done-btn"
